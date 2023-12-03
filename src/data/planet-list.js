89b207b5dc316d2482e238128ab591a3dd92af76
sarker24 <sarker24@gmail.com>
2023-12-03 20:38:26 +0100

    item icon add

---
+++ src/data/planet-list.js
@@ -0,0 +1,51 @@
+export const PLANET_LIST = [
+    {
+        name: "mercury",
+        color: "#DEF4FC",
+        description: "This is a small plannet",
+        rotationTime: 58.6,
+        radius: 234.06
+    },
+    {
+        name: "venus",
+        color: "#DEF4FC",
+        description: "This is a small plannet",
+        rotationTime: 58.6,
+        radius: 234.06
+    },
+    {
+        name: "mars",
+        color: "#F7CC7F",
+        description: "This is a small plannet",
+        rotationTime: 58.6,
+        radius: 234.06
+    },
+    {
+        name: "jupitar",
+        color: "#DEF4FC",
+        description: "This is a small plannet",
+        rotationTime: 58.6,
+        radius: 234.06
+    },
+    {
+        name: "saturan",
+        color: "#DEF4FC",
+        description: "This is a small plannet",
+        rotationTime: 58.6,
+        radius: 234.06
+    },
+    {
+        name: "uranus",
+        color: "#DEF4FC",
+        description: "This is a small plannet",
+        rotationTime: 58.6,
+        radius: 234.06
+    },
+    {
+        name: "neptune",
+        color: "#DEF4FC",
+        description: "This is a small plannet",
+        rotationTime: 58.6,
+        radius: 234.06
+    }
+]
--- src/screens/home.js
+++ src/screens/home.js
@@ -3,12 +3,34 @@ import React from 'react'
 import Text from '../components/text/text'
 import PlanetHeader from '../components/planet-header'
 import { colors } from '../theme/colors'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, FlatList, View } from 'react-native'
+import { PLANET_LIST } from '../data/planet-list'
+import { spacing } from '../theme/spacing'
+import { AntDesign } from '@expo/vector-icons';
 
 export default function Home() {
   return (
    <SafeAreaView style={styles.container}>
     <PlanetHeader />
+    <FlatList
+    contentContainerStyle={styles.list}
+    data={PLANET_LIST}
+    //keyExtractor= {(item, index) => item.name}
+    keyExtractor= {(item) => item.name}
+    renderItem= {({item})=> {
+     return(
+      <View style={styles.item}>
+         <View style={{flexDirection: 'row', alignItems: 'center'}}>
+         <View style={[styles.circle,{backgroundColor: item.color}]} /> 
+         <Text style={styles.itemName}>{item.name}</Text>
+         </View>
+         <AntDesign name="right" size={20} color={colors.white} />
+      </View>
+       
+     )
+    }}
+    ItemSeparatorComponent={()=><View style={styles.separator}/>}
+     />
  </SafeAreaView>
   )
 }
@@ -17,7 +39,30 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: colors.black
+    },
+    item: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginTop: 20,
+        paddingLeft: 20,
+        justifyContent: 'space-between'
+        
+    },
+    list: {
+        padding: spacing[4]
+    },
+    circle:{
+        width: 20, height: 20, borderRadius: 10, marginRight:10
+    },
+    itemName: {
+        textTransform: 'capitalize'
+    },
+    separator:{
+      borderBottomColor: colors.white,
+      borderWidth:0.2,
+      marginTop:10
     }
 })
     
     
+
